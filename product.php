b95cc9eb77c241655329893aa16281d60293dbc7
Murad Nurmukhamedov <mns130691@gmail.com>
2020-07-20 19:49:26 +0300

    add own properties to class Product

--- product.php
+++ product.php
@@ -1,6 +1,12 @@
 <?php
 
 class Product extends Db_object {
-    
+    protected static $db_table = 'products';
+    protected static $db_table_fields = array('product_id', 'product_name', 'cost');
+
+
+    public $product_id,
+          $product_name,
+          $cost;
 }
 ?>
