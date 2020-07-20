bd33e5b88434e9a1f9d75078599f906fc3875ebc
Murad Nurmukhamedov <mns130691@gmail.com>
2020-07-20 19:41:51 +0300

    add comments and add create method

--- db_object.php
+++ db_object.php
@@ -2,8 +2,8 @@
 
 class Db_object {
 
-   public static function find_by_query($sql)
-   {
+    public static function find_by_query($sql)
+    {
       global $database;
       $result_set = $database->query($sql);
       $object_array = array();
@@ -12,19 +12,19 @@ class Db_object {
       }
       return $object_array;
 
-   }
+    }
 
-
-   public static function find_by_id($id)
-   {
+    public static function find_by_id($id)
+    {
       global $database;
       $the_result_array = static::find_by_query("SELECT * FROM ".static::$db_table." WHERE id = {$id}");
       return !empty($the_result_array) ? array_shift($the_result_array) : false;
-   }
+    }
 
-
-   protected static function instantiation($the_record)
-   {
+    
+#### Create an instantiation method 
+    protected static function instantiation($the_record)
+    {
       $called_class = get_called_class();
       $the_object = new $called_class;
       foreach ($the_record as $the_attribute => $value) {
@@ -33,18 +33,19 @@ class Db_object {
          }
       }
       return $the_object;
-   }
+    }
 
-
-   protected function has_the_attribute($the_attribute)
-   {
+#### Create an has_the_attribute method 
+    protected function has_the_attribute($the_attribute)
+    {
       $object_properties = get_object_vars($this);
       return array_key_exists($the_attribute, $object_properties);
-   }
+    }
 
 
-   protected function properties()
-   {
+#### Get the properties from db to object 
+    protected function properties()
+    {
       $properties = array();
       foreach (static::$db_table_fields as $db_field) {
          if (property_exists($this, $db_field)) {
@@ -52,19 +53,40 @@ class Db_object {
          }
          return $properties;
       }
-   }
+    }
 
-
-   protected function clean_properties()
-   {
+#### Clean it
+    protected function clean_properties()
+    {
       global $database;
       $clean_properties = array();
       foreach ($this->properties() as $key => $item) {
          $clean_properties[$key] = $database->escape_string($item);
       }
       return $clean_properties;
-   }
+    }
 
+    
+#### CRUD methods
+    public function create()
+    {
+      global $database;
+
+      $properties = $this->clean_properties();
+      $sql = "INSERT INTO " . static::$db_table . "(" . implode("," , array_keys($properties)) . ")";
+      $sql.= " VALUES ";
+      $sql.= "(' " . implode("','" , array_values($properties)) . " ')";
+
+      if ($database->query($sql)) {
+         $this->id = $database->the_insert_id();
+         return true;
+     } else {
+         echo "query failed";
+         return false;
+     }
+
+   }
+    
 
 }
 
