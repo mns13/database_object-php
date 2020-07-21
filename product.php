7ad4431cca6a5471e8ab6a8afb97fe2b0ed595fe
Murad Nurmukhamedov <mns130691@gmail.com>
2020-07-21 16:45:56 +0300

    add comment in product class

--- product.php
+++ product.php
@@ -2,11 +2,11 @@
 
 class Product extends Db_object {
     protected static $db_table = 'products';
-    protected static $db_table_fields = array('product_id', 'product_name', 'cost');
+    protected static $db_table_fields = array('product_id', 'product_name', 'cost'); // from database table called 'products'
 
 
     public $product_id,
-          $product_name,
-          $cost;
+           $product_name,
+           $cost;
 }
 ?>
